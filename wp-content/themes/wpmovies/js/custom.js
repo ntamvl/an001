9db9fb4b2104e563917104d6c8686bbff57e7d6a
Tam Nguyen <nguyentamvinhlong@gmail.com>
2015-07-17 11:55:07 +0700

    update layout to fixed 800

--- wp-content/themes/wpmovies/js/custom.js
+++ wp-content/themes/wpmovies/js/custom.js
@@ -4,19 +4,18 @@ $(function() {
 
   $('#player-container').find('iframe').each(function(){
       // var currentIframeWidth = $(this).width();
-      $(this).attr("width", "550");
-      $(this).attr("height", "320");
+      // $(this).attr("width", "550");
+      // $(this).attr("height", "320");
 
-      $(this).load( function(){
-        $(this).contents().find('div').attr("width", "550");
-      });
+      // $(this).load( function(){
+      //   $(this).contents().find('div').attr("width", "550");
+      // });
 
-      $(this).contents().find('body').css({'width' : '550px'});
+      // $(this).contents().find('body').css({'width' : '550px'});
   });
-  // embed
   $('#player-container').find('embed').each(function(){
-    $(this).attr("width", "550");
-    $(this).attr("height", "320");
+    // $(this).attr("width", "550");
+    // $(this).attr("height", "320");
   });
 
 });
