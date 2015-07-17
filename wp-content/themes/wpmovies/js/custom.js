e5b08d84e0ab6187a6e70c0f0ee8c25714d34818
Tam Nguyen <nguyentamvinhlong@gmail.com>
2015-07-17 15:36:35 +0700

    update new local

--- wp-content/themes/wpmovies/js/custom.js
+++ wp-content/themes/wpmovies/js/custom.js
@@ -1,4 +1,16 @@
 $(function() {
   $("#ep_ul").addClass("open");
   $("#ep_ul").children("ul").slideDown()
+
+  $('#player-container').find('iframe').each(function(){
+      // var currentIframeWidth = $(this).width();
+      $(this).attr("width", "550");
+
+      $(this).load( function(){
+        $(this).contents().find('div').attr("width", "550");
+      });
+
+      $(this).contents().find('body').css({'width' : '550px'});
+  });
+
 });
